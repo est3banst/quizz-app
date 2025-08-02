564c89906feb546804213da46991c5e1a5db33ad
est3banst <estebeeen@outlook.com>
2025-08-01 23:38:37 -0300

    fixed type errs:2

--- src/App.tsx
+++ src/App.tsx
@@ -135,7 +135,7 @@ function App() {
           <option value="28">Vehículos</option>
         </select>
         <button 
-        disabled={selection !== null}
+        disabled={selection !== ''}
         type='submit' className='border flex gap-2 hover:bg-blue-400 font-medium items-center border-blue-400 cursor-pointer p-3 rounded-md'>Comenzar 
           <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24">
 	        <path fill="currentColor" d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20z"></path>
